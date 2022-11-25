393fc311caa26d5c996248bbdd12de00add80f5d
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-11-25 17:39:55 +0600

    got products

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,11 +13,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u6euqsi.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const categoryCollection = client.db('horse-trade-sale').collection('categories');
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await categoryCollection.find(query).toArray();
+            res.send(products[0].products)
+            // console.log(query)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
